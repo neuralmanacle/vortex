da00a55134c9ca3480b077252b9e704b1a3b7e98
Arjun Shenoy <arjunshenoy23@gmail.com>
2024-10-12 19:47:25 +0530

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 from pymongo.errors import ConnectionFailure
 import os
 
-MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://arjunshenoy23:OXnpTwUdvwXcRzBr@cluster0.wgymm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://arjunshenoy23:OXnpTwUdvwXcRzBr@cluster0.wgymm.mongodb.net/?retryWrites=true&w=majority&ssl=false")
 
 client = MongoClient(MONGO_URI)
 
@@ -15,5 +15,5 @@ except ConnectionFailure:
 
 db = client["vortex"]
 
-items_collection = db["items"]
+item_collection = db["items"]
 clock_in_collection = db["clock_in_records"]
